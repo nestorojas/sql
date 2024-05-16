c1b0488c9764e86eafc5d52c324b8620e8057f4d
Nestor R <nestor.rojas@live.com>
2024-05-16 12:46:20 -0400

    Update homework_3.sql

--- 03_homework/homework_3.sql
+++ 03_homework/homework_3.sql
@@ -2,14 +2,24 @@
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
-
+SELECT v.vendor_id,v.vendor_name, COUNT(vba.vendor_id) AS num_booth_rentals
+FROM vendor v
+INNER JOIN vendor_booth_assignments vba ON v.vendor_id = vba.vendor_id
+GROUP BY v.vendor_id, v.vendor_name
+ORDER BY num_booth_rentals DESC;
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-
+SELECT c.customer_zip,c.customer_last_name, c.customer_first_name,
+  CAST(SUM(cp.cost_to_customer_per_qty * cp.quantity) AS INTEGER) AS amount_spent
+FROM customer c
+INNER JOIN customer_purchases cp ON c.customer_id = cp.customer_id
+GROUP BY c.customer_id, c.customer_last_name, c.customer_first_name, c.customer_zip
+HAVING SUM(cp.cost_to_customer_per_qty * cp.quantity) >= 2000
+ORDER BY amount_spent;
 
 
 --Temp Table
@@ -23,7 +33,28 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+--create the table from the original
+CREATE TEMPORARY TABLE temp.new_vendor (
+    vendor_id INTEGER PRIMARY KEY,
+    vendor_name varchar(45) NOT NULL,
+    vendor_type varchar(45) NOT NULL,
+    vendor_owner_first_name varchar(45) NOT NULL,
+    vendor_owner_last_name varchar(45) NOT NULL,
+    UNIQUE (vendor_id),
+    UNIQUE (vendor_name)
+  );
 
+ -- Insert data from the original vendor table
+INSERT INTO temp.new_vendor (vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+SELECT vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name
+FROM vendor;
+ 
+ -- Insert the new 10th vendor 
+
+INSERT INTO temp.new_vendor (vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES ('Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
+SELECT vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name
+FROM temp.new_vendor;
 
 
 -- Date
@@ -32,9 +63,22 @@ VALUES(col1,col2,col3,col4,col5)
 HINT: you might need to search for strfrtime modifers sqlite on the web to know what the modifers for month 
 and year are! */
 
+SELECT c.customer_id,
+       STRFTIME('%m', cp.market_date) AS purchase_month,
+       STRFTIME('%Y', cp.market_date) AS purchase_year
+FROM customer_purchases cp
+INNER JOIN customer c ON cp.customer_id = c.customer_id;
+
 /* 2. Using the previous query as a base, determine how much money each customer spent in April 2019. 
 Remember that money spent is quantity*cost_to_customer_per_qty. 
 
 HINTS: you will need to AGGREGATE, GROUP BY, and filter...
 but remember, STRFTIME returns a STRING for your WHERE statement!! */
 
+SELECT c.customer_id,
+       SUM(cp.quantity * cp.cost_to_customer_per_qty) AS total_spent
+FROM customer_purchases cp
+INNER JOIN customer c ON cp.customer_id = c.customer_id
+WHERE STRFTIME('%m', cp.market_date) = '04'  -- Filter for April
+  AND STRFTIME('%Y', cp.market_date) = '2019'  -- Filter for 2019
+GROUP BY c.customer_id;
